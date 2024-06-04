93373abe60608c0e5b5203496f6952eb16f07ce2
KevinJSalazar <kejosala@espol.edu.ec>
2024-06-03 19:23:55 -0500

    Funciones ventana agregar vehiculo

--- Grupo_10/src/main/java/ec/edu/espol/model/Pruebas.java
+++ Grupo_10/src/main/java/ec/edu/espol/model/Pruebas.java
@@ -22,11 +22,11 @@ public class Pruebas {
         List<Vehiculo> vehiculos = new SimpleLinkedList<>();
 
         // Agregar algunos vehículos a la lista
-        vehiculos.addLast(new Vehiculo("AAA111", "Toyota", "Corolla", 20000, 15000.5));
-        vehiculos.addLast(new Vehiculo("BBB222", "Toyota", "Camry", 25000, 12000.0));
-        vehiculos.addLast(new Vehiculo("CCC333", "Honda", "Civic", 18000, 20000.0));
-        vehiculos.addLast(new Vehiculo("DDD444", "Honda", "Accord", 27000, 10000.0));
-        vehiculos.addLast(new Vehiculo("EEE555", "Ford", "Fiesta", 15000, 30000.0));
+//        vehiculos.addLast(new Vehiculo("AAA111", "Toyota", "Corolla", 20000, 15000.5));
+//        vehiculos.addLast(new Vehiculo("BBB222", "Toyota", "Camry", 25000, 12000.0));
+//        vehiculos.addLast(new Vehiculo("CCC333", "Honda", "Civic", 18000, 20000.0));
+//        vehiculos.addLast(new Vehiculo("DDD444", "Honda", "Accord", 27000, 10000.0));
+//        vehiculos.addLast(new Vehiculo("EEE555", "Ford", "Fiesta", 15000, 30000.0));
 
         // Filtrar los vehículos
         List<Vehiculo> vehiculosFiltrados = filtrarVehiculos(vehiculos, "Toyota", "Corolla", 15000, 30000, 10000, 20000);
